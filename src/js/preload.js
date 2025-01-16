3d7e6798aee12cd906bc9e5c6caa49fc6421facb
Kiran Ojha <kiranojha1226@gmail.com>
2025-01-16 12:13:00 +0800

    fix: prevent flash of white screen

--- src/js/preload.js
+++ src/js/preload.js
@@ -1,3 +1,8 @@
 (function () {
-    ThemeManager.preloadTheme();
+    const theme = localStorage.getItem('theme') || 'light';
+    document.documentElement.setAttribute('data-theme', theme);
+    if (theme === 'dark' ||
+        (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark-theme');
+    }
 })();
